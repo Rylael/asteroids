fa2dc8473c6ce8bbcce9744f0ce53ac406d318fd
Rylael <daniel.kovesdi@gmail.com>
2025-03-02 23:25:58 +0100

    cleaned main.py, added gitignore

--- main.py
+++ main.py
@@ -12,11 +12,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        
+        screen.fill("black")
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
