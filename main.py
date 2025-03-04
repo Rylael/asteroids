9c1c9f693d8124ccd34df4e5aba4e52c49176d22
Rylael <daniel.kovesdi@gmail.com>
2025-03-04 22:53:13 +0100

    added movement

--- main.py
+++ main.py
@@ -3,19 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     Clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     
+    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = Clock.tick(60) / 1000
 
