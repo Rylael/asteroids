096bee97142f0964ed3cf4173ce114b67ea5ff1d
Rylael <daniel.kovesdi@gmail.com>
2025-03-07 18:41:50 +0100

    added collision to circleshape.py and main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -32,6 +33,10 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for obj in asteroids:
+            if player.collision(obj):
+                sys.exit("Game Over!")
 
         screen.fill("black")
 
