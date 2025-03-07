59ade28bd55d9c10650a5d8932d5754e382925d1
Rylael <daniel.kovesdi@gmail.com>
2025-03-07 22:00:22 +0100

    added shots, fixed shooting orientation

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_MOVE_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_MOVE_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED
+from shot import Shot
 import pygame
 
 class Player(CircleShape):
@@ -33,9 +34,17 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_w]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
     
     def move(self, dt):
         self.position += pygame.Vector2(0, -1).rotate(self.rotation) * PLAYER_MOVE_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        return shot
+      
